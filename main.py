a5e93cdfb0add853915bbb16665ec0e4dadf7e09
Gozto <gostic.adam@gmail.com>
2024-02-03 11:41:24 +0100

    Book name and book price scraped

--- main.py
+++ main.py
@@ -14,5 +14,12 @@ else:
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-for heading in soup.find_all('a'):
-    print(heading.text.strip())
+book_HTML_elements = soup.find_all('a', href=True, title=True)
+book_price_elements = soup.find_all('p', class_='price_color')
+
+
+book_names = [name['title'] for name in book_HTML_elements]
+book_prices = [price.text for price in book_price_elements]
+
+for name, price in zip(book_names, book_prices):
+    print(f'Book: {name}, price: {price[1:]}')
